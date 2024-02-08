d595e41b87b96649c64a74a0cd8be9d2df75dab6
AX19BC <nejcviddmar@gmail.com>
2024-02-08 17:48:38 +0100

    Add notes to the display and add them colors

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ class App {
     constructor() {
         this.notes = [];
 
-        console.log("app works!");
+        this.$placeholder = document.querySelector('#placeholder');
         this.$form = document.querySelector("#form");
+        this.$notes = document.querySelector('#note');
         this.$noteTitle = document.querySelector("#note-title");
         this.$formButtons = document.querySelector("#form-buttons");
         this.$noteText = document.querySelector("#note-text");
@@ -47,17 +48,44 @@ class App {
         this.$form.classList.remove('form-open');
         this.$noteTitle.style.display = 'none';
         this.$formButtons.style.display = 'none';
+        this.$noteTitle.value = '';
+        this.$noteText.value = '';
     }
 
     addNote(note) {
         const newNote = {
             title: note.title,
             text: note.text,
-            color: 'white',
+            color: this.randomColorGenerator(),
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
         this.notes = [...this.notes, newNote];
-        console.log(this.notes);
+        this.displayNotes();
+        this.closeForm();
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0;
+        this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+        this.$notes.innerHTML = this.notes.map(note =>
+            `
+            <div style="background: ${note.color};" class="note">
+                <div class="${note.title && 'note-title'}">${note.title}</div>
+                <div class="note-text">${note.text}</div>
+                <div class="toolbar-container">
+                    <div class="toolbar">
+                        <img class="toolbar-color" src="https://icon.now.sh/palette">
+                        <img class="toolbar-delete" src="https://icon.now.sh/delete">
+                    </div>
+                </div>
+            </div>
+        `).join("");
+    }
+
+
+
+    randomColorGenerator() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
     }
 }
 
