499f9f6bf84a36a65ed33399b704ef3faed9421c
AX19BC <nejcviddmar@gmail.com>
2024-02-11 10:08:41 +0100

    add localStorage

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class App {
     constructor() {
-        this.notes = [];
+        this.notes = JSON.parse(localStorage.getItem('notes')) || [];
         this.title = '';
         this.text = '';
         this.id = '';
@@ -18,6 +18,7 @@ class App {
         this.$modalCloseButton = document.querySelector(".modal-close-button");
         this.$colorTooltip = document.querySelector("#color-tooltip");
 
+        this.render();
         this.addEvenetListeners();
     }
 
